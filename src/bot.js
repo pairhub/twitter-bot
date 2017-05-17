1ea349742ff7f1096ddec1349b8ac0d02d5c4bc0
spences10 <spences10apps@gmail.com>
2017-05-17 14:25:42 +0100

    Initial files 🕺🚀

---
+++ src/bot.js
@@ -0,0 +1,13 @@
+const Twit = require('twit')
+
+const bot = new Twit(config)
+
+bot.post('statuses/update', {
+  status: 'hello world!'
+}, (err, data, response) => {
+  if (err) {
+    console.log(err)
+  } else {
+    console.log(`${data.text} tweeted!`)
+  }
+})
